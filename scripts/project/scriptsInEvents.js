e74614f06c15f2ea1e0af1c5cd756189081821db
Sandi Pratama <yue_xie@outlook.com>
2021-08-30 17:53:31 +0700

    up new version

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+		async Maincode_Event35_Act1(runtime, localVars)
+		{
+			alert("Still on construction!");
+		},
+
+		async Maincode_Event36_Act1(runtime, localVars)
+		{
+			alert("Still on construction!");
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
